c7057bf8d1326684079cfa342f24c2dbda5ad520
Calin Don <calin.don@gmail.com>
2019-02-14 19:07:36 +0200

    Allow customization for demo

--- demo/03-install-presslabs-stack.sh
+++ demo/03-install-presslabs-stack.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+: ${VARIABLE:="presslabs/stack"}
+: ${PRESET:="gke"}
+: ${PRESETS_LOCATION:="https://raw.githubusercontent.com/presslabs/stack/master/presets"}
+
 set -x
+
+kubectl create ns presslabs-stack
+
+kubectl label namespace presslabs-stack certmanager.k8s.io/disable-validation=true
 
 helm repo add presslabs https://presslabs.github.io/charts
 
 helm repo update
 
-helm upgrade -i presslabs presslabs/stack --namespace presslabs-sys \
-    -f https://raw.githubusercontent.com/presslabs/stack/master/presets/gke.yaml
+helm upgrade -i stack $CHART --namespace presslabs-stack \
+    -f "${PRESETS_LOCATION}/${PRESET}.yaml"
