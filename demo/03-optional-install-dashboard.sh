23f7ccf82ddbbab36184e763a1199883df8f1a68
Calin Don <calin.don@gmail.com>
2019-03-08 12:30:53 +0200

    Make dashboard install optional

--- demo/03-optional-install-dashboard.sh
+++ demo/03-optional-install-dashboard.sh
@@ -5,3 +5,4 @@ kubectl create clusterrolebinding kubernetes-dashboard \
     --clusterrole cluster-admin \
     --serviceaccount=kube-system:kubernetes-dashboard
 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
