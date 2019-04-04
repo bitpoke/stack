bc3431ff5588ea677ad96c658e25ce764d946a27
Calin Don <calin.don@gmail.com>
2019-04-04 22:13:31 +0100

    Switch tiller storage backend to secret

--- demo/01-install-helm.sh
+++ demo/01-install-helm.sh
@@ -7,4 +7,7 @@ kubectl create clusterrolebinding tiller \
     --clusterrole cluster-admin \
     --serviceaccount=kube-system:tiller
 
-helm init --service-account tiller --history-max 10 --wait
+helm init --service-account tiller \
+    --history-max 10 \
+    --override 'spec.template.spec.containers[0].command'='{/tiller,--storage=secret}' \
+    --wait
