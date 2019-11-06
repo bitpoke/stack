c4e1efebae1277c72554fab5d939f6e809795035
amecea <flavius@presslabs.com>
2019-11-06 18:30:26 +0200

    Add tiller and fix entrypoint

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -3,9 +3,10 @@
 set -e
 
 # install manfiests and wait to be ready
-kubectl apply -f /manifests/
+kubectl apply --validate=false -f /manifests/
 kubectl wait --for condition=established --timeout=${TIMEOUT:-60s} -f /manifests/
 
 
 # run helm to install the stack
-helm upgrade -i /charts/stack --reuse-values --namespace ${NAMESPACE:-default} -f /config/values.yaml --wait
+helm init --client-only
+helm upgrade -i stack /charts/stack --reuse-values --namespace ${NAMESPACE:-default} -f /config/values.yaml --wait
