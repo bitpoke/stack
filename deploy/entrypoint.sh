22313d1f161e6d8c35157d2fa48e5df1ea6100ae
amecea <flavius@presslabs.com>
2019-11-07 18:11:12 +0200

    Use supervisord to run tiller

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+# stop the tiller if running
+function stop_tiller {
+    supervisord ctl shutdown
+}
+trap stop_tiller EXIT
+
 # install manfiests and wait to be ready
 kubectl apply --validate=false -f /manifests/
 kubectl wait --for condition=established --timeout=${TIMEOUT:-60s} -f /manifests/
 
-
 # run helm to install the stack
-helm init --client-only
-helm upgrade -i stack /charts/stack --reuse-values --namespace ${NAMESPACE:-default} -f /config/values.yaml --wait
+helm upgrade -i stack /charts/stack --reuse-values --namespace ${NAMESPACE:-presslabs-system} -f /config/*.yaml --wait
