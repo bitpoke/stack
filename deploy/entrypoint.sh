63dc6d2042d0a96cd8631ba96be791655b36b564
amecea <flavius@presslabs.com>
2019-11-11 17:29:47 +0200

    Set mysql topology secret one time

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -8,9 +8,26 @@ function stop_tiller {
 }
 trap stop_tiller EXIT
 
-# install manfiests and wait to be ready
+# install manfiests and wait to be ready (e.g. crds)
 kubectl apply --validate=false -f /manifests/
 kubectl wait --for condition=established --timeout=${TIMEOUT:-60s} -f /manifests/
 
+# create mysql-operator orchestrator topology secret
+orc_secret_name=stack-mysql-operator-topology-credentials
+if ! kubectl get secret $orc_secret_name; then
+    cat <<EOF | kubectl create -f-
+apiVersion: v1
+kind: Secret
+metadata:
+    name: ${orc_secret_name}
+    namespace: ${NAMESPACE:-presslabs-system}
+type: Opaque
+data:
+    TOPOLOGY_PASSWORD: $(echo -n ${ORCHESTRATOR_PASSWORD:-$(tr -dc '_A-Z-a-z-0-9' < /dev/urandom  | head -c31)} | base64 )
+    TOPOLOGY_USER: b3JjaGVzdHJhdG9y
+EOF
+fi
+
 # run helm to install the stack
-helm upgrade -i stack /charts/stack --reuse-values --namespace ${NAMESPACE:-presslabs-system} -f /config/*.yaml --wait
+helm upgrade -i stack /charts/stack --reuse-values --namespace ${NAMESPACE:-presslabs-system} -f /config/*.yaml \
+     --set mysql-operator.orchestrator.secretName=${orc_secret_name} --wait
